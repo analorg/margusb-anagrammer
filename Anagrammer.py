47e43e57d47a33a8fbee268d9da3c517b5e1cf4f
margusb <margusb@gmail.com>
2019-02-27 23:17:19 +0200

    Anagrammer with caching

---
+++ Anagrammer.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+import datetime
+import sys
+import pickle
+import os
+from collections import defaultdict
+
+
+def read_dictionary_file(filepath):
+    try:
+        with open(filepath,'rb') as f:
+            contents = f.read()
+        contents = contents.decode('utf-8', errors = 'ignore')    
+        contents = contents.replace(' ', '|').splitlines()
+    except:
+        contents = []
+    return contents
+
+
+def encode_anagrams(word_list):
+    enc = dict((w,''.join(sorted(w.lower().replace('|','')))) for w in word_list)
+    return enc
+
+
+def encode_anagrams_inverted(word_list):
+    enc_rev = defaultdict(list)
+    
+    enc = dict((w,''.join(sorted(w.lower().replace('|','')))) for w in word_list)
+ 
+    for k, v in enc.items():
+        enc_rev[v].append(k) 
+    return enc_rev
+
+
+def get_cache_filename(filename):
+    return os.path.splitext(os.path.basename(filename))[0]
+
+
+def get_cache_loc(filename):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)),filename+'.pkl') 
+
+
+def encode_anagrams_cache(word_list, cache_file, recache = False):
+    cache = os.path.isfile(cache_file)   
+
+    if not cache or recache:
+        enc = encode_anagrams(word_list)
+        try:
+            with open(cache_file,'wb') as f:
+                pickle.dump(enc, f)
+            return enc
+        except:
+            print('Error creating cache file:', cache_file)
+
+    try:
+        with open(cache_file,'rb') as f:
+            enc = pickle.load(f)
+    except:    
+           enc = encode_anagrams(word_list)
+           #enc = defaultdict() #fallback
+    #print(enc)
+    return enc
+
+
+def find_anacrams(word, encoded_dict):
+    try:
+        word_encoded = ''.join(sorted(word.lower().replace(' ','')))
+        anagrams = [k.replace('|',' ') for k, v in encoded_dict.items() if v == word_encoded]
+        return anagrams
+    except KeyError:
+        return []
+
+def find_anacrams_inverted(word, encoded_dict):
+    try:
+        word_encoded = ''.join(sorted(word.lower().replace(' ','')))
+        return encoded_dict[word_encoded]
+    except KeyError:
+        return []
+
+
+def anagrammer(filepath, word):
+    contents = read_dictionary_file(filepath)
+    
+    # Live transformations (k,v => k, encode(v))
+    #dict_encoded_anagram = encode_anagrams(contents)
+    #anagrams = find_anacrams(word, dict_encoded_anagram)
+
+    # Live transformations (k,v => encode(v),[k])
+    #dict_encoded_anagram = encode_anagrams_inverted(contents)
+    #anagrams = find_anacrams_inverted(word, dict_encoded_anagram)
+
+    # Cached transformations
+    cache_filename = get_cache_filename(filepath)
+    cache_file = get_cache_loc(cache_filename)
+    dict_encoded_anagram = encode_anagrams_cache(contents, cache_file, recache = False)
+    anagrams = find_anacrams(word, dict_encoded_anagram)
+     
+    #if word in anagrams:
+    #    anagrams.remove(word)
+    return anagrams
+    
+
+def main(argv=None):
+    res = ''
+    start = datetime.datetime.now()
+
+    filepath = 'data/lemmad.txt'
+    #filepath = 'data/google-10000-english.txt'
+    word = 'test'
+    
+    if len(argv) >= 2:
+        filepath = argv[0] or ''
+        word = argv[1] or '' 
+    
+    anagrams = anagrammer(filepath, word)
+
+    stop = datetime.datetime.now()
+    delta = stop - start
+ 
+    if len(anagrams)>0:
+        res = str(delta.microseconds) + ',' + ','.join(anagrams)
+ 
+    print(res)
+    #sys.stdout.write(anagram_res)
+
+if __name__ == "__main__": 
+    main(sys.argv[1:])
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# anagrammer
+# Anagrammer
+
 Multi-word anagram solver
+
+## Installation / linux (Ubuntu)
+
+You need to have:
+ - python3
+ - git
+
+
+Download latest build
+
+```
+$ git clone https://github.com/margusb/nlp.git Anagrammer
+```
+
+Make script executable
+```
+$ chmod a+x anagrammer.py
+```
+
+## Usage
+
+```
+anagrammer {path} {word}
+$ python anagrammer /path/to/dictionary/file test
+```
+
+The above example would output:
+```
+duration,list,of,anagrams,found
+86744,sett,test,tste
+```
